f54cbec78eec7cf1f2808dd3608d19c1e3e44227
idmacleod <idmacleod@gmail.com>
2020-05-08 14:03:22 +0100

    MVP complete

--- app.js
+++ app.js
@@ -5,8 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const registerAnimalForm = document.querySelector('#register-animal');
     registerAnimalForm.addEventListener('submit', registerAnimal);
 
-    const clearButton = document.querySelector('#clear');
-    clearButton.addEventListener('click', clear);
+    const clearButton = document.querySelector('#clearRegister');
+    clearButton.addEventListener('click', clearRegister);
+
+    const typeOptions = {
+        'Cat': '🐈',
+        'Dog': '🐕',
+    }
+
+    const vetOptions = ['Doctor Dolittle', 'James Herriott', 'Vetty McVetVet'];
+    
+    const addOptions = function (select, options) {
+        for (optionText of options) {
+            const option = document.createElement('option');
+            option.value = optionText;
+            option.textContent = optionText;
+            select.appendChild(option);
+        }
+    }
+    
+    addOptions(document.querySelector('#type-select'), Object.keys(typeOptions));
+    addOptions(document.querySelector('#vet-select'), vetOptions);
 
 });
 
@@ -17,22 +36,31 @@ const registerAnimal = function (event) {
 }
 
 const addToRegister = function (target) {
-    console.log(target.name.value);
     const register = document.querySelector('#register');
     const listItem = document.createElement('li');
     const table = document.createElement('table');
-    const tableRow = document.createElement('tr');
-    const tableHeader = document.createElement('th');
-    tableHeader.textContent = 'Name';
-    const tableData = document.createElement('td');
-    tableData.textContent = target.name.value;
-    tableRow.appendChild(tableHeader);
-    tableRow.appendChild(tableData);
-    table.appendChild(tableRow);
+    addRow(table, 'Name:', target.name.value);
+    addRow(table, 'Date of Birth:', target.dob.value);
+    addRow(table, 'Type:', target.type.value);
+    addRow(table, 'Contact Tel:', target.contact.value);
+    addRow(table, 'Notes:', target.notes.value);
+    addRow(table, 'Vet:', target.vet.value);
     listItem.appendChild(table);
     register.appendChild(listItem);
 }
 
-const clear = function (event) {
-    console.log(event);
+const addRow = function (table, header, data) {
+    const tableRow = document.createElement('tr');
+    const tableHeader = document.createElement('th');
+    tableHeader.textContent = header;
+    const tableData = document.createElement('td');
+    tableData.textContent = data;
+    tableRow.appendChild(tableHeader);
+    tableRow.appendChild(tableData);
+    table.appendChild(tableRow);
+}
+
+const clearRegister = function () {
+    const register = document.querySelector('#register');
+    register.innerHTML = '';
 }
