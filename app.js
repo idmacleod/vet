84515b0b8af9c6013f4a261853fd61220f959135
idmacleod <idmacleod@gmail.com>
2020-05-08 17:53:06 +0100

    Lots of styling, delete button on each card, more types of pet

--- app.js
+++ app.js
@@ -5,18 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const clearRegisterButton = document.querySelector('#clear-register-button');
     clearRegisterButton.addEventListener('click', clearRegister);
-
-    const typeOptions = {
-        'Cat': '🐈',
-        'Dog': '🐕',
-    }
-
-    const vetOptions = ['Doctor Dolittle', 'James Herriott', 'Vetty McVetVet'];
     
     addOptions(document.querySelector('#type-select'), Object.keys(typeOptions));
     addOptions(document.querySelector('#vet-select'), vetOptions);
 
 });
+
+const typeOptions = {
+    'Cat': '🐈',
+    'Dog': '🐩',
+    'Rabbit': 🐇,
+    'Mouse': '🐁',
+    'Rat': '🐀',
+    'Parrot': '🦜',
+    'Otter': '🦦',
+    'Duck': '🦆'
+    'Chicken': '🐓'
+    'Tiger': '🐅',
+    'Dragon': '🐉'
+}
+
+const vetOptions = ['Doctor Dolittle', 'James Herriott', 'Nurse Joy'];
 
 const addOptions = function (select, options) {
     for (optionText of options) {
@@ -32,15 +41,16 @@ const registerAnimal = function (event) {
     const register = document.querySelector('#register');
     const listItem = document.createElement('li');
     const table = document.createElement('table');
-    addRowToTable(table, 'Name:', event.target.name.value);
+    const header = makeCardHeader(event.target);
     addRowToTable(table, 'Date of Birth:', event.target.dob.value);
     addRowToTable(table, 'Type:', event.target.type.value);
     addRowToTable(table, 'Contact Tel:', event.target.contact.value);
     addRowToTable(table, 'Notes:', event.target.notes.value);
     addRowToTable(table, 'Vet:', event.target.vet.value);
+    listItem.appendChild(header);
     listItem.appendChild(table);
     register.appendChild(listItem);
-    this.reset();
+    // this.reset();
 }
 
 const addRowToTable = function (table, header, data) {
@@ -57,4 +67,25 @@ const addRowToTable = function (table, header, data) {
 const clearRegister = function () {
     const register = document.querySelector('#register');
     register.innerHTML = '';
+}
+
+const makeCardHeader = function (target) {
+    const header = document.createElement('div');
+    const badge = document.createElement('h3');
+    const name = document.createElement('h3');
+    const deleteButton = document.createElement('button');
+    badge.textContent = typeOptions[target.type.value];
+    name.textContent = target.name.value;
+    deleteButton.textContent = 'Delete';
+    badge.classList.add('badge');
+    deleteButton.classList.add('delete-btn');
+    deleteButton.addEventListener('click', deleteCard);
+    header.appendChild(badge);
+    header.appendChild(name);
+    header.appendChild(deleteButton);
+    return header;
+}
+
+const deleteCard = function () {
+    this.parentElement.parentElement.remove();
 }
